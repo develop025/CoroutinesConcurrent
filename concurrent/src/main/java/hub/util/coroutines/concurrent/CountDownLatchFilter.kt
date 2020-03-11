826df2ce797bd1b12235a327474cd53cf3ffeef9
android <android@syneforge.com>
2020-03-11 18:55:17 +0200

    - Implemented CountDownLatch

--- concurrent/src/main/java/hub/util/coroutines/concurrent/CountDownLatchFilter.kt
+++ concurrent/src/main/java/hub/util/coroutines/concurrent/CountDownLatchFilter.kt
@@ -3,7 +3,8 @@ package hub.util.coroutines.concurrent
 import kotlinx.coroutines.sync.Mutex
 import java.util.concurrent.atomic.AtomicInteger
 
-open class CountDownLatch(open val count: AtomicInteger) {
+class CountDownLatchFilter(override val count: AtomicInteger, private val filter: Any?) :
+    CountDownLatch(count) {
     private val general = Mutex()
     private val generalLock = Any()
 
@@ -11,16 +12,10 @@ open class CountDownLatch(open val count: AtomicInteger) {
         require(count.get() >= 0) { "count < 0" }
     }
 
-    suspend fun await() {
-        general.lock(generalLock)
-    }
-
-    fun countDown() {
+    override fun countDown(result: Any?) {
+        super.countDown()
         count.decrementAndGet()
-        if (count.get() <= 0)
+        if (count.get() <= 0 || (filter != null && filter == result))
             general.unlock(generalLock)
     }
-
-    fun getCount() = count.get()
-    open fun countDown(result: Any?) {}
 }
---
+++ concurrent/src/main/java/hub/util/coroutines/concurrent/DynamicCountDownLatchFilter.kt
@@ -0,0 +1,59 @@
+package hub.util.coroutines.concurrent
+
+import android.util.Log
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+class DynamicCountDownLatchFilter(
+    private val filter: Any?,
+    private val dispatcher: CoroutineDispatcher
+) {
+    private val general = Mutex()
+    private val generalLock = Any()
+    private val resultMutex = Mutex()
+
+    @Volatile
+    private var result: Any? = null
+
+    private val jobs = mutableListOf<Job>()
+    private val suspendListMutex = mutableListOf<Mutex>()
+
+//    override fun countDown(result: Any?) {
+//        super.countDown()
+//        count.decrementAndGet()
+//        if (count.get() <= 0 || (filter != null && filter == result))
+//            general.unlock(generalLock)
+//    }
+
+    fun start(suspendFunction: () -> Unit) {
+        "start".logd()
+        val job = CoroutineScope(dispatcher).launch {
+            "launch".logd()
+            val result = suspendFunction.invoke()
+            if (result == filter) {
+                "result == filter".logd()
+                dispatcher.cancel()
+            }
+        }
+        "jobs.add".logd()
+        jobs.add(job)
+    }
+
+    suspend fun setResult(r: Any) {
+        resultMutex.withLock {
+            result = r
+        }
+    }
+
+    suspend fun await() {
+        if (suspendListMutex.count() > 0)
+            general.lock(generalLock)
+        else
+            throw IllegalAccessException()
+    }
+}
+
+private fun String.logd() {
+    Log.d("myapp", this)
+}
